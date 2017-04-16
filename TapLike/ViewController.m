c16d5d60adf91e3370e51dea7904eb67bbaca6ef
李亚坤 <285577079@qq.com>
2017-04-16 23:17:44 +0800

    添加基本点赞动画

--- TapLike/ViewController.m
+++ TapLike/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "DMHeartFlyView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, assign) CGFloat heartSize;
+@property (nonatomic, strong) NSTimer *burstTimer;
 
 @end
 
@@ -16,14 +20,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.heartSize = 36;
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.view.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(showTheLove)];
+    [self.view addGestureRecognizer:tapGesture];
+    
+    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self
+                                                                                                   action:@selector(longPressGesture:)];
+    longPressGesture.minimumPressDuration = 0.2;
+    [self.view addGestureRecognizer:longPressGesture];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)showTheLove
+{
+    DMHeartFlyView* heart = [[DMHeartFlyView alloc]initWithFrame:CGRectMake(0, 0, _heartSize, _heartSize)];
+    [self.view addSubview:heart];
+    CGPoint fountainSource = CGPointMake(20 + _heartSize/2.0, self.view.bounds.size.height - _heartSize/2.0 - 10);
+    heart.center = fountainSource;
+    [heart animateInView:self.view];
 }
 
+- (void)longPressGesture:(UILongPressGestureRecognizer *)longPressGesture
+{
+    switch (longPressGesture.state) {
+        case UIGestureRecognizerStateBegan:
+            self.burstTimer = [NSTimer scheduledTimerWithTimeInterval:0.2
+                                                               target:self
+                                                             selector:@selector(showTheLove)
+                                                             userInfo:nil
+                                                              repeats:YES];
+            break;
+        case UIGestureRecognizerStateEnded:
+            [self.burstTimer invalidate];
+            self.burstTimer = nil;
+            break;
+        default:
+            break;
+    }
+}
 
 @end
